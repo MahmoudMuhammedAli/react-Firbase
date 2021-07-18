61746a5778abed5f70bb93cebfb2f839d0f9eb42
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-07-18 05:36:27 +0200

    Get rid of boiler plate

--- fire-react/src/firebase.js
+++ fire-react/src/firebase.js
@@ -4,10 +4,13 @@ import "firebase/auth"
 
 
 const app = firebase.intializeApp({
-    apiKey:process.env.REACT_APP_APP_ID,
-    authDomain:process.env.REACT_APP_AUTH_DOM,
-    projectId:process.env.REACT_APP_PROJID,
-    storageBucket:process.env.REACT_APP_STORAGE_BUCKET, ,
+    apiKey: process.env.REACT_APP_APP_ID,
+    authDomain: process.env.REACT_APP_AUTH_DOM,
+    projectId: process.env.REACT_APP_PROJID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET, ,
     messagingSenderId: process.env.REACT_APP_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
 })
+
+export const auth = app.auth()
+export default app

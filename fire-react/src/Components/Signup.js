41557a2f4bfd49f60e82a83d9d165b8939808f5b
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2021-07-18 07:57:17 +0200

    Signup almost done

--- fire-react/src/Components/Signup.js
+++ fire-react/src/Components/Signup.js
@@ -1,15 +1,39 @@
-import React, {useRef} from "react";
-import { Form, Button, Card } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Button, Card , Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+
 export default function Signup() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth;
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("passwords don't match");
+    }
+    try {
+      setError("");
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("failed to create a new account");
+    }
+    setLoading(false)
+  }
   return (
     <>
       <Card className="mt-5">
         <Card.Body>
           <h2 className="text-center mb-4">sign Up</h2>
-          <Form>
+          {error&& <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -22,7 +46,9 @@ export default function Signup() {
               <Form.Label>password confirm</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button className="w-100 mt-4" type="submit">Sign Up</Button>
+            <Button  disabled={loading} className="w-100 mt-4" type="submit">
+              Sign Up
+            </Button>
           </Form>
         </Card.Body>
       </Card>
--- fire-react/src/firebase.js
+++ fire-react/src/firebase.js
@@ -3,11 +3,11 @@ import "firebase/auth"
 
 
 
-const app = firebase.intializeApp({
+const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_APP_ID,
     authDomain: process.env.REACT_APP_AUTH_DOM,
     projectId: process.env.REACT_APP_PROJID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET, ,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET, 
     messagingSenderId: process.env.REACT_APP_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
 })
